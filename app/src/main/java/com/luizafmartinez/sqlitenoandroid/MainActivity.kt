57e67842e5e97d8f374d16e3e3b2f366626ed945
alxmart <alxmartinez@gmail.com>
2023-12-11 17:20:12 -0300

    Project and Class DatabaseHelper created.

---
+++ app/src/main/java/com/luizafmartinez/sqlitenoandroid/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.luizafmartinez.sqlitenoandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.luizafmartinez.sqlitenoandroid.database.DatabaseHelper
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val dbHelper = DatabaseHelper(this)
+    }
+}
---
+++ app/src/main/java/com/luizafmartinez/sqlitenoandroid/database/DatabaseHelper.kt
@@ -0,0 +1,22 @@
+package com.luizafmartinez.sqlitenoandroid.database
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class DatabaseHelper(context: Context): SQLiteOpenHelper(
+    // 1) Contexto (context-> to use for locating paths to the database.)
+    // 2) Nome do B.D.      3) CursorFactory      4) Versão do B.D.
+    context, "loja.db", null, 1
+) {
+    override fun onCreate(db: SQLiteDatabase?) {
+
+
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+
+
+
+    }
+}
