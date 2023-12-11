7c3bc9ed54063d327ff37ce045471756746c03be
alxmart <alxmartinez@gmail.com>
2023-12-11 17:21:56 -0300

    Project and Class DatabaseHelper created.

--- app/src/main/java/com/luizafmartinez/sqlitenoandroid/database/DatabaseHelper.kt
+++ app/src/main/java/com/luizafmartinez/sqlitenoandroid/database/DatabaseHelper.kt
@@ -5,8 +5,10 @@ import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
 class DatabaseHelper(context: Context): SQLiteOpenHelper(
+
     // 1) Contexto (context-> to use for locating paths to the database.)
     // 2) Nome do B.D.      3) CursorFactory      4) Versão do B.D.
+
     context, "loja.db", null, 1
 ) {
     override fun onCreate(db: SQLiteDatabase?) {
