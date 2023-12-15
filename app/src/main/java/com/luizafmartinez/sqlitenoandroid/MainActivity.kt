e1bdd658fb1e50f06a37ee4d0ca6aae49e31636a
alxmart <alxmartinez@gmail.com>
2023-12-14 22:50:33 -0300

    Added code: onCreate and onUpgrade.

--- app/src/main/java/com/luizafmartinez/sqlitenoandroid/MainActivity.kt
+++ app/src/main/java/com/luizafmartinez/sqlitenoandroid/MainActivity.kt
@@ -2,16 +2,27 @@ package com.luizafmartinez.sqlitenoandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.luizafmartinez.sqlitenoandroid.database.DatabaseHelper
 
 class MainActivity : AppCompatActivity() {
+
+    private val bancoDados by lazy {
+        DatabaseHelper( this )
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val dbHelper = DatabaseHelper(this)
-
+        try {
+            bancoDados.writableDatabase.execSQL(
+              "INSERT INTO produtos VALUES (null,'Notebook Acer', 'Descricao...');"
+            )
+            Log.i("info_db", "Sucesso ao inserir.")
+        } catch (e: Exception) {
+            Log.i("info_db", "Sucesso ao inserir.")
+        }
 
     }
 }
--- app/src/main/java/com/luizafmartinez/sqlitenoandroid/database/DatabaseHelper.kt
+++ app/src/main/java/com/luizafmartinez/sqlitenoandroid/database/DatabaseHelper.kt
@@ -3,22 +3,35 @@ package com.luizafmartinez.sqlitenoandroid.database
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import android.util.Log
 
 class DatabaseHelper(context: Context): SQLiteOpenHelper(
 
+    context, "loja.db", null, 3
+
     // 1) Contexto (context-> to use for locating paths to the database.)
     // 2) Nome do B.D.      3) CursorFactory      4) Versão do B.D.
-
-    context, "loja.db", null, 1
 ) {
     override fun onCreate(db: SQLiteDatabase?) {
 
+        Log.i("info_db", "Executou onCreate.")
 
+        val sql = "CREATE TABLE IF NOT EXISTS produtos (" +
+                "id_produto integer not null primary KEY AUTOINCREMENT," +
+                "titulo varchar(100)," +
+                "descricao text" +
+                ");"
+        try {
+            db?.execSQL( sql )
+            Log.i("info_db", "Sucesso ao criar a tabela.")
+        } catch (e: Exception) {
+            // e.message  // Exibir na tela o erro
+            e.printStackTrace()
+            Log.i("info_db", " ERRO ao criar a tabela.")
+        }
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
-
-
-
+        Log.i("info_db", " Executou onUpgrade.")
     }
 }
